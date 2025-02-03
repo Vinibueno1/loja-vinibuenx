4303908f7372368129444b96540af428dc858a21
Vinibueno1 <buenozviniciushenrique@gmail.com>
2025-02-03 15:00:12 -0300

    Criado projeto MVC

---
+++ LojaVinibuenx/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LojaVinibuenx.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
