53a3da6e6c01c0021372f03fc9a6789adaf4834e
Vinibueno1 <buenozviniciushenrique@gmail.com>
2025-02-06 16:48:52 -0300

    Desenvolvimento site

---
+++ Banco de Dados/vinielo.sql
@@ -0,0 +1,34 @@
+-- Criar banco
+CREATE DATABASE viniloja;
+
+use viniloja;
+
+CREATE TABLE Categoria
+(
+	Id			int not null identity primary key,
+	Nome		varchar(50) not null unique,
+	Foto		varchar(200),
+	ExibirHome	bit
+);
+
+CREATE TABLE Produto
+(
+	Id			int not null identity primary key,
+	Nome		varchar(100) not null,
+	Descricao	varchar(1000),
+	CategoriaId	int	not null,
+	ValorCusto  numeric(10,2),
+	ValorVenda  numeric(10,2) not null,
+	QtdeEstoque int not null default 0,
+	Destaque	bit default 0,
+	foreign key (CategoriaId) references Categoria (Id)
+);
+
+CREATE TABLE ProdutoFoto
+(
+	Id			int not null identity primary key,
+	ProdutoId	int not null,
+	Foto		varchar(200) not null,
+	Descricao	varchar(100),
+	foreign key (ProdutoId) references Produto(Id)
+);
